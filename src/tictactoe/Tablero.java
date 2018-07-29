9c49b4fe9083b9a5f1c84e13dc5206c34cac3343
mtsluna <mtsluna@gmail.com>
2018-07-29 16:14:11 -0300

    Logica de juego terminada

--- src/tictactoe/Tablero.java
+++ src/tictactoe/Tablero.java
@@ -12,10 +12,12 @@ package tictactoe;
 public class Tablero {    
     
     public char [][] tablero;
+    public int turno;
     
     public Tablero(){
         
         tablero = new char [3][3];
+        turno = (int)(Math.random()*2);
         
     }
     
@@ -39,6 +41,42 @@ public class Tablero {
                 }
             }
         }
+        
+    }
+    
+    public char verificarVictoria(char turno){
+        
+        
+        if (tablero[0][0] == turno){
+            if (tablero[0][1] == turno){
+                if (tablero[0][2] == turno){
+                    return turno;
+                }
+            }
+            if (tablero[1][1] == turno){
+                if (tablero[2][2] == turno){
+                    return turno;
+                }
+            }
+            if (tablero[1][0] == turno){
+                if (tablero[2][0] == turno){
+                    return turno;
+                }
+            }
+        }
+        if (tablero[2][2] == turno){
+            if (tablero[1][2] == turno){
+                if (tablero[0][2] == turno){
+                    return turno;
+                }
+            }
+            if (tablero[2][1] == turno){
+                if (tablero[2][0] == turno){
+                    return turno;
+                }
+            }
+        }
+        return 'N';
         
     }
     
--- src/tictactoe/Vista.java
+++ src/tictactoe/Vista.java
@@ -8,6 +8,7 @@ package tictactoe;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -22,6 +23,7 @@ public class Vista extends JFrame implements ActionListener{
     private JButton [][] botones;
     private JButton [] botonesMenu;
     private Tablero tablero;
+    private boolean turno;
     
     public Vista(){
         
@@ -38,14 +40,14 @@ public class Vista extends JFrame implements ActionListener{
         
         getContentPane().setPreferredSize(new Dimension(ancho,alto));
         
-        añadirBotones();
+        añadirBotones();        
         
         pack();        
     }
     
     public void inicializarBotones(int i, int j){
         
-        botones[i][j] = new JButton();
+        botones[i][j] = new JButton(new ImageIcon("src/resources/botonVacio.png"));
         botones[i][j].setName(i+""+j);
         botones[i][j].addActionListener(this);
         add(botones[i][j]); 
@@ -71,42 +73,101 @@ public class Vista extends JFrame implements ActionListener{
             }
         }
         
-        inicializarBotonesMenu();
+        añadirBotonesMenu();
         
     }
     
-    public void inicializarBotonesMenu(){
+    public void inicializarBotonesMenu(int i){
+                
+        botonesMenu[i] = new JButton();
+        botonesMenu[i].setName(""+i);
+        botonesMenu[i].addActionListener(this);
         
+        if (i == 0){
+            botonesMenu[i].setIcon(new ImageIcon("src/resources/botonReinicio.png"));
+        }
+        
+        add(botonesMenu[i]);
+        
+    }
+    
+    public void añadirBotonesMenu(){        
         int x = 0;
         int anchoBoton = ancho/tablero.tablero[0].length;
         int altoBoton = 25;
         
         for (int i = 0; i < tablero.tablero[0].length; i++){
-            botonesMenu[i] = new JButton();
-            botonesMenu[i].setName(""+i);
-            botonesMenu[i].addActionListener(this);
+            inicializarBotonesMenu(i);
             botonesMenu[i].setBounds(x, 0, anchoBoton, altoBoton);
-            add(botonesMenu[i]);
             
             x += anchoBoton;
         }
         
     }
     
+    public void turno(){
+        if (tablero.turno == 0){
+            turno = false;
+        }
+        else{
+            if (tablero.turno == 1){
+                turno = true;
+            }
+        }
+    }
+    
     public void actionPerformed(ActionEvent e){
         
         //Manejo de botones juego
         if(((JButton)e.getSource()).getName().length() == 2){
-            System.out.println("Botón juego: "+((JButton)e.getSource()).getName());
+            String posicion = ((JButton)e.getSource()).getName();
+            int x = Integer.parseInt(""+posicion.charAt(0)), y = Integer.parseInt(""+posicion.charAt(1));
+            if (turno){
+                tablero.tablero[x][y] = 'X';
+                botones[x][y].setIcon(new ImageIcon("src/resources/botonX.png"));
+                turno = false;
+                if (tablero.verificarVictoria('X') == 'X'){
+                    System.out.println("X WIN");
+                    usoTablero(false);
+                }
+                else{
+                    System.out.println("Todavia no hay ganadores");
+                }
+                
+            }
+            else{
+                tablero.tablero[x][y] = 'O'; 
+                botones[x][y].setIcon(new ImageIcon("src/resources/botonO.png"));
+                turno = true;
+                if (tablero.verificarVictoria('O') == 'O'){
+                    System.out.println("O WIN");
+                    usoTablero(false);
+                }
+                else{
+                    System.out.println("Todavia no hay ganadores");
+                }
+            }
+            tablero.mostrarConsolaTablero();
         }        
         //Manejo de botones menu
         else{
             if (((JButton)e.getSource()).getName().length() == 1){
                 System.out.println("Boton menu: "+((JButton)e.getSource()).getName());
+                int [] posicionBotonesMenu;
             }
         }
     }
     
+    public void usoTablero(boolean uso){
+        
+        for (int i = 0; i < tablero.tablero.length; i++){
+            for (int j = 0; j < tablero.tablero[0].length; j++){
+                botones[i][j].setEnabled(uso);
+            }
+        }
+            
+    }
     
+
     
 }
